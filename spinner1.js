31d625d060d4454fa8bf0c3a2d6712a5c4a6f388
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-06-13 17:50:27 +0000

    refactored spinner1 code

--- spinner1.js
+++ spinner1.js
@@ -1,15 +1,29 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
+///// REFACTORED CODE:
 
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
+let r = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ']
+let timer = 100
 
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
+for (let i = 0; i < r.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(r[i]);
+  }, timer);
+  timer+= 200
+}
 
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 700);
+/// PREVIOUS CODE:
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\   ');
+// }, 700);
